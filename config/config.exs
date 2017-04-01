9e566c6e1d063fe6b367d8f3e361108bf323a36e
Andrew Forward <aforward@gmail.com>
2017-04-01 18:52:42 -0400

    Upgrade to custom mix_test_watch to better support DB tests

--- config/config.exs
+++ config/config.exs
@@ -2,6 +2,17 @@ use Mix.Config
 
 config :stripe_callbacks, ecto_repos: [StripeCallbacks.Repo]
 
+if Mix.env == :dev do
+  config :mix_test_watch,
+    setup_tasks: [
+      "ecto.drop --quiet",
+      "ecto.create --quiet",
+      "ecto.migrate",
+    ],
+    ansi_enabled: :ignore,
+    clear: true
+end
+
 if (File.exists?("./config/#{Mix.env}.exs")) do
   import_config "#{Mix.env}.exs"
 end
